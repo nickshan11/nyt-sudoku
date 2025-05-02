971413573a9472002c2b50aec14121be42317616
Nick Shan <shanboyuan@gmail.com>
2025-05-01 22:48:00 -0700

    get current day sudoku

---
+++ load_nyt.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import sync_playwright
+
+
+def scrape_sudoku_cells():
+    with sync_playwright() as p:
+        # Launch a headless Chromium browser
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        # Go to the NYT easy Sudoku page
+        page.goto("https://www.nytimes.com/puzzles/sudoku/hard")
+
+        # Wait until at least one cell has been injected
+        page.wait_for_selector('div[data-testid^="sudoku-cell-"]')
+
+        # Query all the cell divs
+        cells = page.query_selector_all('div[data-testid^="sudoku-cell-"]')
+        grid = []
+        # Print out their attributes
+        for row in range(9):
+            temp = []
+            for col in range(9):
+                value = cells[row*9+col].get_attribute("aria-label")
+                if value == "empty":
+                    value = 0
+                temp.append(int(value))
+            grid.append(temp)
+
+        browser.close()
+        return grid
+
+
+if __name__ == "__main__":
+    print(scrape_sudoku_cells())
